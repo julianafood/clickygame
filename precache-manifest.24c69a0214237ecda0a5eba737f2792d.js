0e36d2283046cd5a9e114a08b7a0aa7bed944de9
julianafood <chojgy@gmail.com>
2019-05-13 10:04:47 -0400

    Updates

---
+++ precache-manifest.24c69a0214237ecda0a5eba737f2792d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d9764f54e1981d2b4c619edc45b6ab",
+    "url": "/julianafood/clickygame/index.html"
+  },
+  {
+    "revision": "81788f4b69e9ed4624cf",
+    "url": "/julianafood/clickygame/static/css/main.14886a6f.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/julianafood/clickygame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "81788f4b69e9ed4624cf",
+    "url": "/julianafood/clickygame/static/js/main.3680da9a.chunk.js"
+  },
+  {
+    "revision": "d89de6adf429f8483f63",
+    "url": "/julianafood/clickygame/static/js/runtime~main.0f5bbdb4.js"
+  }
+]);
